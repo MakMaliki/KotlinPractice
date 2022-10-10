bd604b51148c10e39228e7f97ea942ae31ff2c01
faisalhakim <malikfaisalhakim@gmail.com>
2022-10-10 22:55:32 +0200

    ArrayList,Collections, Expressions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,10 @@
+import kotlin.math.max
+
 fun main(args: Array<String>) {
 
+//Break Keyword
 
+    //Terminate numbers
 
+    //Continue terminates currents step and forward to next step
 }
---
+++ src/main/kotlin/whileDoWhileLOOP.kt
@@ -0,0 +1,5 @@
+class whileDoWhileLOOP {
+    fun main(){
+
+    }
+}
