32f19fd557363f3e8d265016273b19979321037b
faisalhakim <malikfaisalhakim@gmail.com>
2022-10-13 23:34:52 +0200

    Classes,OOP

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,65 @@
 
 fun main(args: Array<String>) {
 
-//  var randomInt = setOf(42.55,55,66.77,77.32,88,99,22,33,44.5,1)
-//
-//  var subset = randomInt.filter { it in 0..50 }
-//
-//
-//  var names = listOf("Malik","Hakim","Habib","Jawad","Nawab")
-//
-//  var sortedList = names.sortedBy { it[it.length-1] }
-//  println(sortedList)
 
-  //println(subset)
+    //An object is an instance of a class
 
-  var numbers = listOf(10, 14, 13, 11, 122, 88, 9, 92, 23, 43, 56)
+    val myCar = Car()
+    val yourCar:Car = Car()
 
-println(numbers)
- var newNumbers= numbers.map {
-    if (it %2 ==0){
-      it*2
-    }else{
-      it/2
+    println("Print Object My car and change its attribute model and topspeed")
+
+    myCar.model ="Toyota"
+    myCar.model ="BMW"
+
+    myCar.start()
+    myCar.start()
+
+    myCar.drive(22)
+    yourCar.drive(22)
+
+
+
+    var animal1 = Animal();
+
+    animal1.topSpeed =564
+    animal1.name = "silva"
+    animal1.run()
+
+    var dog = Animal();
+
+    dog.topSpeed =333
+    dog.name ="Money"
+    dog.run()
+
+
+}
+
+//a class is a blueprint for components and Objects
+//Class has variables , Methods
+
+//Name should start with Capital letter
+class Car{
+    var model:String?=null
+    var topSpeed =100
+
+    fun start(){
+
+        println("Starting the model $model")
     }
-  }.filter { it>55 }
-  println(newNumbers)
+
+    fun drive(speed:Int){
+
+        println("Driving at the speed of $speed")
+    }
+}
+
+class Animal{
+    var name:String = ""
+    var topSpeed:Int = 0
+
+    fun run(){
+        println("$name  speed is $topSpeed")
+    }
 
 }
