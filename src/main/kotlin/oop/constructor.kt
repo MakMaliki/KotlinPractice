03e56513e020d0eb2e3d3b78f7c02e99efaf8dca
Malik Faisal Hakim <malikfaisalhakim@gmail.com>
2022-10-22 20:21:05 +0200

    Create constructor.kt

---
+++ src/main/kotlin/oop/constructor.kt
@@ -0,0 +1,36 @@
+
+fun main(args: Array<String>){
+    //Constructors
+
+  //  defines set of variable that are available at the creation of an object
+
+    val myCar = Car("BMW",2300)
+
+    val yourCar = Car("Merceds")
+
+    val hisCar = Car()
+    
+
+
+}
+//Different Ways to create a class
+
+class Car{
+
+    constructor(){
+        model ="No Model"
+        topSpeed =160
+    }
+
+    constructor(newModel:String){
+    model = newModel
+    }
+
+    constructor(newModel: String,topSpeed:Int){
+        this.topSpeed=topSpeed
+        model =newModel
+    }
+
+    var model:String?=null
+    var topSpeed= 100
+}
