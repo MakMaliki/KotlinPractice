2917b2dd8c32b524d0b7c979a1564c94fd42949d
malikfaisal <malik-faisal.hakim@ihaus.de>
2022-10-25 12:09:09 +0200

    Generic

---
+++ src/main/kotlin/coroutines/firstAppCoroutines.kt
@@ -0,0 +1,36 @@
+package coroutines
+/**Threads are sresource Intensive
+Coroutines are lightweight threads
+              -> use thread pools
+
+Coroutines Simplify async code, callbacks and Synchronisation
+ Coroutines Simle Syntax
+ Can pause and resume at any time, on  number of threads
+
+
+ Basic Concepts Coroutines
+
+ Scope ->  Create and run coroutines , provide lifecycle events, pausing, stoping ,running manage here
+
+ Context -> The scope provide context in which coroutine runs, state of coroutine
+
+ Suspending Functions -> Functions that can be run in a coroutine, , Can be suspended
+
+ Jobs -> A handle on a coroutine, access lifecycle events,
+
+ Deferred -> a FUTURE RESULT OF A COROUTINE, When it finished u can get result
+
+ Dispatcher -> Manage which threads the coroutines runs on
+
+ Error Handling
+
+
+
+
+
+
+
+
+ **/
+class firstAppCoroutines {
+}
