7fbefb9ef8806a10509601c92575e747993bd4dd
malikfaisal <malik-faisal.hakim@ihaus.de>
2022-10-28 16:21:39 +0200

    Generic

--- src/main/kotlin/lambdafunctionNew.kt
+++ src/main/kotlin/lambdafunctionNew.kt
@@ -1,14 +1,48 @@
+import functions.printMyName
+
 /**
  * Pass a function as a paramter to another function
  * */
 
 fun main(){
 
-    var list = (1..20).toList()
-    println(list)
-
-    list = list.filter{ it%2==0}
-    println(list)
+//showNameLambda("Malik")
+//printMyName("Hakim")
 
 
+    enhancedMessage("Malik"){22 }
+
+}
+
+fun sum(a:Int,b:Int):Int{
+    return a+b
+}
+
+val sumLambda:(Int,Int)->Int = {a,b -> a+b}
+
+
+fun calculateCatAge(age:Int):Int = age*7
+
+//VAL lambdaName(params) :-> TYPE = {paramsList -> CodeBody}
+val calcAge:(Int)->Int={a ->a*7}
+
+fun showName(name:String){
+    println(name)
+}
+
+
+var showNameLambda:(String)->Unit ={name->
+    print(name)
+}
+
+
+// Trailing Lambda
+//We can add a function as a parameter to another Function
+
+//If the last parameter of a function is a function,
+//Then a lambda expression passed as correspondant could be passed outside as Argument
+
+//The passed function in lambda could be anything but it should return the request result type
+fun enhancedMessage(message:String, funAsAparameter: ()-> Int){
+    println("Your name is $message and the user age is ${funAsAparameter()}")
 }
