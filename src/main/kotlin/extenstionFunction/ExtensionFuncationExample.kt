0c322d3822839745585a0a5c480badd1ec4135ff
malikfaisal <malik-faisal.hakim@ihaus.de>
2022-11-10 10:27:34 +0100

    Extension Functions

--- src/main/kotlin/extenstionFunction/ExtensionFuncationExample.kt
+++ src/main/kotlin/extenstionFunction/ExtensionFuncationExample.kt
@@ -2,14 +2,11 @@ package extenstionFunction
 
 
 fun main(){
-
     var list = mutableListOf(1,3,4,6)
     println(list)
     list.swap(0,2)
 
     println(list)
-
-
 }
 
 fun MutableList<Int>.swap(index1: Int,index2:Int){
